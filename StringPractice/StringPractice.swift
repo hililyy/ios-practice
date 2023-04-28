51bed9c27b63e3f81b8b4728e72651349632980e
Lily <76806444+hililyy@users.noreply.github.com>
2023-04-28 10:37:25 +0900

    Update StringPractice.swift

--- StringPractice/StringPractice.swift
+++ StringPractice/StringPractice.swift
@@ -10,8 +10,15 @@ public class Model {
 var dataList: [Model] = []
 
 for i in 0...30 {
+    
+    var month = "\(Int.random(in: 1...12))"
+    month = month.count == 1 ? "0\(month)" : month
+    
+    var day = "\(Int.random(in: 1...28))"
+    day = day.count == 1 ? "0\(day)" : day
+    
     let data = Model()
-    data.date = "20230\(Int.random(in: 1...9))0\(Int.random(in: 1...9))"
+    data.date = "2023\(month)\(day)"
     data.title = "제목\(i)"
     
     dataList.append(data)
@@ -20,13 +27,13 @@ for i in 0...30 {
 var newList: [[Model]] = []
 
 
+
 // 풀이 --------------------------------------------
 
 var dicList: [String: [Model]] = [:]
 
 for data in dataList {
-    let arr = Array(data.date)
-    let sliceMonth = String(arr.suffix(4).prefix(2))
+    let sliceMonth = "\(data.date.suffix(4).prefix(2))"
     
     if dicList["\(sliceMonth)"] == nil {
         dicList["\(sliceMonth)"] = []
@@ -37,7 +44,18 @@ for data in dataList {
 
 newList = dicList.map {
     $0.value.sorted(by: { $0.date > $1.date })}
-    .sorted { $0[0].date < $1[0].date
-}
+    .sorted { $0.first?.date ?? "" > $1.first?.date ?? "" }
 
-print(newList)
+
+
+// 출력 --------------------------------------------
+for new in newList {
+    print("---\(new[0].date.suffix(4).prefix(2))월---")
+    
+    for n in new {
+        print("""
+        \(n.title)
+        \(n.date)
+        """)
+    }
+}
